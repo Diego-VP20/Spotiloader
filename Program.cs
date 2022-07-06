14b34d4b21de45bd0a47f788ece4e4571bd160a3
Diego-VP20 <diego.varela@stud.edubs.ch>
2022-07-06 19:38:29 +0200

    Init

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace Spotiloader
+{
+    public static class Program
+    {
+        private static ServiceProvider ConfigureServices()
+        {
+            return new ServiceCollection()
+                .AddLogging(l => l.AddSerilog())
+                .BuildServiceProvider();
+        }
+        
+        public static void Main()
+        {
+            
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Spotiloader
+A program that helps you download spotify songs, playlists and whole artists.
