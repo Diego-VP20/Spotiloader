f7c5ce279654ad5cde0f2830377d109426bd7a64
Diego-VP20 <69905156+Diego-VP20@users.noreply.github.com>
2022-07-10 18:21:58 +0200

    Refactor and use newtonsoft

--- Config/ConfigManager.cs
+++ Config/ConfigManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using Newtonsoft.Json;
+using Spectre.Console;
 
 namespace Spotiloader.Config;
 
@@ -6,39 +7,96 @@ public static class ConfigManager
 {
     private static readonly string ConfigFile = AppDomain.CurrentDomain.BaseDirectory + "config.json";
 
-    public static bool IsConfigFile()
-    {
-        return File.Exists(ConfigFile);
-    }
+    private static bool IsConfigFile() => File.Exists(ConfigFile);
 
-    private static async Task<bool> IsValidConfig()
+    private static async Task CreateOrRepairConfigFile()
     {
-        try
-        {
-            await JsonSerializer.DeserializeAsync<SpotifyApplication>(File.OpenRead(ConfigFile));
-            return true;
-        }catch(JsonException)
-        {
-            return false;
-        }
-    }
-
-    public static async Task<SpotifyApplication> GetConfig()
-    {
-        if (IsConfigFile() && await IsValidConfig()) return (await JsonSerializer.DeserializeAsync<SpotifyApplication>(File.OpenRead(ConfigFile)))!;
-    
         var config = new SpotifyApplication
         {
             ClientId = "",
             ClientSecret = ""
         };
-    
-        await using var file = File.Create(ConfigFile);
-        await JsonSerializer.SerializeAsync(file, config);
-        await file.DisposeAsync();
-    
-        File.SetAttributes(ConfigFile, File.GetAttributes(ConfigFile) | FileAttributes.Hidden);
-    
-        return (await JsonSerializer.DeserializeAsync<SpotifyApplication>(File.OpenRead(ConfigFile)))!;
+        
+        await File.WriteAllTextAsync(ConfigFile, JsonConvert.SerializeObject(config, Formatting.Indented));
+    }
+
+    private static async Task<bool> IsValidConfigAsync()
+    {
+        try
+        {
+            JsonConvert.DeserializeObject<SpotifyApplication>(await File.ReadAllTextAsync(ConfigFile));
+            return true;
+        }
+        catch (JsonSerializationException)
+        {
+            return false;
+        }
+    }
+
+    private static async Task<SpotifyApplication> GetConfigAsync()
+    {
+        return JsonConvert.DeserializeObject<SpotifyApplication>(await File.ReadAllTextAsync(ConfigFile));
+    }
+
+    public static async Task<SpotifyApplication> InitializeConfigAsync()
+    {
+        var config = new SpotifyApplication
+        {
+            ClientId = "",
+            ClientSecret = ""
+        };
+        
+        await AnsiConsole.Status()
+            .AutoRefresh(true)
+            .StartAsync("Checking configuration...", async ctx =>
+            {
+                ctx.Spinner(Spinner.Known.Circle);
+                ctx.SpinnerStyle(Style.Parse("green"));
+
+                var isConfigFile = IsConfigFile();
+                var isValidConfig = await IsValidConfigAsync();
+                    
+                await Task.Delay(3000);
+                    
+                if (!isConfigFile)
+                {
+                    ctx.Status("[orange3 bold]First launch detected[/]");
+                    await Task.Delay(1000);
+                    ctx.Status("[green bold]Creating config file...[/]");
+                    await Task.Delay(1000);
+                    await CreateOrRepairConfigFile().ContinueWith(_ =>
+                    {
+                        ctx.Status("[green bold]Done.[/]");
+                        Task.Delay(1000);
+                    });
+                }
+
+                if (!isValidConfig)
+                {
+                    ctx.Status("[orange3 bold]Invalid config file, repairing config...[/]");
+                    await Task.Delay(1000);
+
+                    await CreateOrRepairConfigFile().ContinueWith(_ =>
+                    {
+                        ctx.Status("[green bold]Done.[/]");
+                        Task.Delay(1000);
+                    });
+                }
+
+                ctx.Status("[green bold]Loading Config file...[/]");
+                await Task.Delay(1000);
+
+                var configTask = GetConfigAsync();
+                
+                await configTask.ContinueWith(_ =>
+                {
+                    ctx.Status("[green bold]Done.[/]");
+                    Task.Delay(1000);
+                });
+
+                config = await configTask;
+            });
+        
+        return config;
     }
 }
--- Config/SpotifyApplication.cs
+++ Config/SpotifyApplication.cs
@@ -1,7 +1,12 @@
-﻿namespace Spotiloader.Config;
+﻿using Newtonsoft.Json;
+
+namespace Spotiloader.Config;
 
 public class SpotifyApplication
 {
-    public string ClientId { get; set; } = null!;
-    public string ClientSecret { get; set; } = null!;
+    [JsonProperty(PropertyName = "ClientId", Required = Required.Always)]
+    public string ClientId { get; set; } = "";
+    
+    [JsonProperty(PropertyName = "ClientSecret", Required = Required.Always)]
+    public string ClientSecret { get; set; } = "";
 }
