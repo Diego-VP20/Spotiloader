efa807e5064159d311db2b813a8eb7d29d01930c
Diego-VP20 <69905156+Diego-VP20@users.noreply.github.com>
2022-07-12 01:42:29 +0200

    Add a menu item to test reflection

---
+++ Menu/Options/DownloadSongOption.cs
@@ -0,0 +1,22 @@
+﻿using Spotiloader.API;
+
+namespace Spotiloader.Menu.Options;
+
+public class DownloadSongOption : IMenuItem
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "Download Song";
+
+    private readonly SpotifyService _spotifyService;
+
+    public DownloadSongOption(SpotifyService service)
+    {
+        _spotifyService = service;
+    }
+    
+    public async Task Action()
+    {
+        _spotifyService.Test();
+        await Task.Delay(1000);
+    }
+}
