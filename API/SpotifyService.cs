c3ed6ddf5a47f2b921031f9a12182dfd69fa423d
Diego-VP20 <69905156+Diego-VP20@users.noreply.github.com>
2022-07-12 01:40:59 +0200

    Add SpotifyService with test method

---
+++ API/SpotifyService.cs
@@ -0,0 +1,19 @@
+﻿using Spectre.Console;
+using Spotiloader.Config;
+
+namespace Spotiloader.API;
+
+public class SpotifyService
+{
+    private SpotifyApplication _spotifyApiSettings = null!;
+    
+    public void Init(SpotifyApplication config)
+    {
+        _spotifyApiSettings = config;
+    }
+
+    public void Test()
+    {
+        AnsiConsole.MarkupLine($"[darkcyan]{_spotifyApiSettings.ClientId}/{_spotifyApiSettings.ClientSecret}[/]");
+    }
+}
