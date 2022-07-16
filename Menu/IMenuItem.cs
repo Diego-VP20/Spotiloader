7bb6e57267594ed3a3c28d671708257cada3db57
Diego-VP20 <69905156+Diego-VP20@users.noreply.github.com>
2022-07-16 17:17:51 +0200

    Add logic to MenuItems

--- Menu/IMenuItem.cs
+++ Menu/IMenuItem.cs
@@ -1,5 +1,8 @@
 ﻿namespace Spotiloader.Menu;
 
+[AttributeUsage(AttributeTargets.Class)]
+public class RequiresSpotifyAuthentication : Attribute{}
+
 public interface IMenuItem
 {
     /// <summary>
@@ -11,6 +14,11 @@ public interface IMenuItem
     /// Name of the menu item that will be displayed.
     /// </summary>
     public string Name { get; set; }
+    
+    /// <summary>
+    /// Sets whether the menu item is enabled or not.
+    /// </summary>
+    public bool Enabled { get; set; }
 
     /// <summary>
     /// Function that well be run when the menu item is selected.
