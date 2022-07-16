3d296dfb284347dc2f8be985c43ba1257cd99d9c
Diego-VP20 <69905156+Diego-VP20@users.noreply.github.com>
2022-07-16 17:17:34 +0200

    Use menu from DI and enable user authentication

---
+++ Menu/Options/UpdateCredentialsOption.cs
@@ -0,0 +1,30 @@
+﻿using Spotiloader.API;
+using Spotiloader.Config;
+
+namespace Spotiloader.Menu.Options;
+
+public class UpdateCredentialsOption : IMenuItem
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "Update Credentials";
+    public bool Enabled { get; set; }
+    private readonly Menu _menu;
+    private readonly SpotifyService _spotifyService;
+    
+    public UpdateCredentialsOption(Menu menu, SpotifyService spotifyService)
+    {
+        _menu = menu;
+        _spotifyService = spotifyService;
+    }
+    
+    public async Task Action()
+    {
+        var newConfig = await ConfigManager.InitializeConfigAsync(updatingConfig: true);
+
+        await _spotifyService.Init(newConfig);
+        
+        _menu.IsAuthenticated = _spotifyService.IsAuthenticated();
+        
+        _menu.LoadOptions();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -13,7 +13,9 @@ namespace Spotiloader
         {
             return new ServiceCollection()
                 .AddLogging(l => l.AddSerilog())
+                .AddSingleton<YoutubeService>()
                 .AddSingleton<SpotifyService>()
+                .AddSingleton<Menu.Menu>()
                 .BuildServiceProvider();
         }
 
@@ -37,32 +39,40 @@ namespace Spotiloader
         
         public static async Task Main()
         {
-            // Init logger configuration
-            InitLoggerConfiguration();
-            
-            // Configure services
-            await using var serviceProvider = ConfigureServices();
-            
-            // Present program banner to user.
-            PresentHeader();
-            
-            // Initialize config. (Create, Validate, Load...)
-            var config = await ConfigManager.InitializeConfigAsync();
-            
-            // Initialize Spotify service class.
-            if(!await InitSpotifyService(serviceProvider, config))
+            try
             {
-                NonGracefulExit($"Failed to initialize Spotify service. Login failed. Update your credentials in {ConfigManager.GetConfigFilePath()} and try again.");
+// Init logger configuration
+                InitLoggerConfiguration();
+            
+                // Configure services
+                await using var serviceProvider = ConfigureServices();
+            
+                // Present program banner to user.
+                PresentHeader();
+            
+                // Initialize config. (Create, Validate, Load...)
+                var config = await ConfigManager.InitializeConfigAsync();
+            
+                // Initialize Spotify service class.
+                var initSpotifyServiceSuccess = await InitSpotifyService(serviceProvider, config);
+
+                // Present header again.
+                PresentHeader(true);
+
+                // Present menu to user.
+                var menu = serviceProvider.GetRequiredService<Menu.Menu>();
+            
+                menu.IsAuthenticated = initSpotifyServiceSuccess;
+            
+                await menu.Run();
+
+                // Exit program.
+                GracefulExit();
             }
-            
-            // Present header again.
-            PresentHeader(true);
-            
-            // Present menu to user.
-            await new Menu.Menu(serviceProvider).Run();
-            
-            // Exit program.
-            GracefulExit();
+            catch (Exception e)
+            {
+                NonGracefulExit(e.Message);
+            }
         }
 
         private static void GracefulExit()
